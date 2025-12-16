14226c2ea44fbd8264b8b42c85f1a96776025962
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-16 22:40:18 +0530

    added new lab3 in the repository

---
+++ src/ex_01_Java_Basics/Lab003_Code3.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Code3
+{
+    static void main() {
+        System.out.print("OK");
+        System.out.println("KO");
+        System.out.println("New line");
+    }
+}
