f6fd2ba24c3c958c980532f71ef3bb849e4146ab
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-19 21:42:45 +0530

    Updated

--- src/ex_01_Java_Basics/Lab_004_Printftableof9.java
+++ src/ex_01_Java_Basics/Lab_004_Printftableof9.java
@@ -2,18 +2,19 @@ package ex_01_Java_Basics;
 
 public class Lab_004_Printftableof9
 {
-    public static void main(String [] args)
+    static void main()
     {
-        int a= 9;
-        System.out.printf("%d x 1 =%d\n" ,a, a* 1);
-        System.out.printf("%d x 2 =%d\n", a ,a* 2);
-        System.out.printf("%d x 3 =%d\n", a ,a* 3);
-        System.out.printf("%d x 4 =%d\n", a ,a* 4);
-        System.out.printf("%d x 5 =%d\n", a ,a* 4);
-        System.out.printf("%d x 6 =%d\n", a ,a* 6);
-        System.out.printf("%d x 7 =%d\n", a ,a* 7);
-        System.out.printf("%d x 8 =%d\n", a ,a* 8);
-        System.out.printf("%d x 9 =%d\n", a ,a* 9);
-        System.out.printf("%d x 10 =%d\n",a, a* 10);
+        int a = 9;
+        System.out.printf("%d x 1 =%d\n", a, a*1);
+        System.out.printf("%d x 2 =%d\n", a, a * 2);
+        System.out.printf("%d x 3 =%d\n", a, a * 3);
+        System.out.printf("%d x 4 =%d\n", a, a * 4);
+        System.out.printf("%d x 5 =%d\n", a, a * 4);
+        System.out.printf("%d x 6 =%d\n", a, a * 6);
+        System.out.printf("%d x 7 =%d\n", a, a * 7);
+        System.out.printf("%d x 8 =%d\n", a, a * 8);
+        System.out.printf("%d x 9 =%d\n", a, a * 9);
+        System.out.printf("%d x 10 =%d\n",a, a * 10);
+
     }
 }
