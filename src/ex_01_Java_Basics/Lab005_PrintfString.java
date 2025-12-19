391282617202e14050ceeacaa5345e50b2878749
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-19 22:00:48 +0530

    program os prinf using %s

---
+++ src/ex_01_Java_Basics/Lab005_PrintfString.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab005_PrintfString {
+    static void main() {
+        String java= ("learning java is important");
+        System.out.printf("%s in todays date" ,java);
+    }
+}
