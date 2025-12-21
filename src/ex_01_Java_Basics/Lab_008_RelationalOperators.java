216b30dfb880d88d1abf6412c239f1adac323e03
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-21 22:46:00 +0530

    Relational operators

---
+++ src/ex_01_Java_Basics/Lab_008_RelationalOperators.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+public class Lab_008_RelationalOperators
+{
+    public static void main(String[] args)
+    {
+        int x = 5 , y=10;
+        System.out.println(x>y);
+        System.out.println(x<y);
+        System.out.println(x==y);
+        System.out.println(x!=y);
+         //Relational Operators12
+
+    }
+
+
+
+}
