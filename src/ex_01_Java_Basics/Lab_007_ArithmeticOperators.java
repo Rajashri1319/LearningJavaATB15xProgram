0e71aa813f8ea3df4da56708d9dca694df8d7530
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-21 22:38:48 +0530

    program os prinf for all datatypes

---
+++ src/ex_01_Java_Basics/Lab_007_ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab_007_ArithmeticOperators
+{
+    public static void main(String[] args)
+    {
+        int a = 10 , b =2;
+        System.out.println("Addition :" + (a+b));
+        System.out.println("Substraction :"+ (a-b));
+        System.out.println("Multiplication :" + (a*b));
+        System.out.println("Division :" + (a/b));
+        System.out.println("Modulus :" + (a%b));
+    }
+}
