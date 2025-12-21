509a43362ef008f017f54c90756d75b8b65b8e43
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-21 22:54:38 +0530

    Logical Operators

---
+++ src/ex_01_Java_Basics/Lab10_LogicalOperator.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab10_LogicalOperator
+{
+    static void main(String[] args) {
+        int age =25;
+        System.out.println(age>18 && age<60);
+        System.out.println(age >30 || age<20);
+        System.out.println(! (age > 18));
+
+    }
+}
