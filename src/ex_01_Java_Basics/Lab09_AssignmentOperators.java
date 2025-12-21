c32f152df38093d01d4d4390a69eda49ecb924f2
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-21 22:51:02 +0530

    Assignment Operators

---
+++ src/ex_01_Java_Basics/Lab09_AssignmentOperators.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab09_AssignmentOperators
+{
+    static void main(String[] args) {
+        int a=10;
+        System.out.println(a+=5);
+        System.out.println(a-=3);
+        System.out.println(a*=2);
+
+    }
+}
