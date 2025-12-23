f59139499eeb9621642ac8ad3f0ec78bc03f01cc
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-23 21:50:00 +0530

    Program: Swap Two Numbers Using Arithmetic Operators Method 1: Using Addition and Subtraction (+, -)

---
+++ src/ex_01_Java_Basics/SwapTwoNumbersusingArithmeticOperators.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basics;
+
+public class SwapTwoNumbersusingArithmeticOperators
+{
+    static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Before Swap :");
+        System.out.println("a :"+a);
+        System.out.println("b :"+b);
+          //Before Swap
+        a=a+b;  //a=10+20 now a=30
+        b=a-b;  //b=30-20 now b=10
+        a=a-b;  //a=30-10 now a=20
+        System.out.println("After Swap ");
+        System.out.println("a :"+a);
+        System.out.println("b :"+b);
+
+
+    }
+}
