5edf6b73dd04e6bbbcfdc114c6138e8502f253e3
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-23 20:42:30 +0530

    ind Remainder when dividing by 10 Input: 157 Output: 7 (using % operator)

---
+++ src/ex_01_Java_Basics/remainderwhendividingby10.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class remainderwhendividingby10
+{
+    static void main(String[] args) {
+        int a=157;
+        int remainder = a%10;
+        System.out.println("The Remainder when dividing by 10 is :"+remainder);
+    }
+}
