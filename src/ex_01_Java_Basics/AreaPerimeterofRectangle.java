dc4a7df94e081c6ef889d9a5e424ee31b92f8842
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-23 21:16:46 +0530

    Calculate Area and Perimeter of Rectangle Input: length=10, width=5 Use: Area = l×w, Perimeter = 2×(l+w)

---
+++ src/ex_01_Java_Basics/AreaPerimeterofRectangle.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class AreaPerimeterofRectangle
+{
+    static void main(String[] args) {
+        int L=10;
+        int W=5;
+        int Area = L*W;
+        int perimeter = 2*(L=W);
+        System.out.println("Length is :"+L);
+        System.out.println("Width is :"+W);
+        System.out.println("The Area is :"+Area);
+        System.out.println("The perimeter is :"+perimeter);
+    }
+}
