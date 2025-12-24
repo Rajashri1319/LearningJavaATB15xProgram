603a477b9107f1f34335c9155b2abbbb6ae19281
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-24 21:15:38 +0530

    2. Find Maximum of Two Numbers Input: a=15, b=25 Output: Maximum = 25

---
+++ src/ex_01_Java_Basics/TernaryMaxof2numbers.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class TernaryMaxof2numbers
+{
+    static void main(String[] args) {
+        int a= 15;  int b=25;
+        int Max= (a>b) ? a : b;
+        System.out.println("The maximum of two numbers is :"+Max);
+
+    }
+}
