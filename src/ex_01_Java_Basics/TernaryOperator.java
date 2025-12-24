7539bd225c094f8ddac5dbc8d2dfd3e55dc1e00f
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-24 21:10:31 +0530

    1. Check if Number is Positive or Negative Use: (num > 0) ? "Positive" : "Negative"

---
+++ src/ex_01_Java_Basics/TernaryOperator.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class TernaryOperator
+{
+    static void main(String[] args) {
+        int num=20;
+        String results = (num>0) ? "Positive" : "Negative";
+        System.out.println("The number is :"+results);
+    }
+}
