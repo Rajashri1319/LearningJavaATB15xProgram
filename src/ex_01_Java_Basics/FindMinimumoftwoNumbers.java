713ccaa7a769a20b96c933e3c6aab203c6550a00
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2025-12-25 15:33:57 +0530

    Find Minimum of Two Numbers

---
+++ src/ex_01_Java_Basics/FindMinimumoftwoNumbers.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class FindMinimumoftwoNumbers
+{
+    static void main(String[] args)
+    {
+        int a =10;
+        int b=30;
+        int min = (a<b) ? a : b;
+        System.out.println("a :"+a);
+        System.out.println("b:"+b);
+        System.out.println("Min of two numbers is :"+min);
+
+
+
+    }
+}
