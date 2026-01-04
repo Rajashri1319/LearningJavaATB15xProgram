49890682800543c33635ba2de15e5e871e7509a4
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2026-01-04 19:16:42 +0530

    Write a program that prints numbers from 1 to 100. However, for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz"

---
+++ src/Coding_Interview_Questions/FizzBuzz_Test.java
@@ -0,0 +1,27 @@
+package Coding_Interview_Questions;
+
+public class FizzBuzz_Test
+{
+    static void main(String[] args)
+        {
+        for(int i= 1; i<=100 ; i++)
+        {
+            if (i%3==0 && i%5==0)
+            {
+                System.out.println("FIZZBUZZ");
+            }
+            else if (i%3==0)
+            {
+                System.out.println("FIZZ");
+            }
+            else if (i%5==0)
+            {
+                System.out.println("BUZZ");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+
+    }
+    }
