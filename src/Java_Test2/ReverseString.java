63e71cb7eb146a6050c921955ba092d1ee348deb
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2026-01-18 18:23:38 +0530

    JAVATEST2

--- src/Java_Test2/ReverseString.java
+++ src/Java_Test2/ReverseString.java
@@ -22,7 +22,7 @@ public class ReverseString {
         String reversed = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+            reversed = reversed + str.charAt(i);
         }
         return reversed;
     }
