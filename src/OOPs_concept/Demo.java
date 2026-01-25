312a1b5c08dcbac4bda3988ae418bc6b717f6497
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2026-01-25 20:32:13 +0530

    Use constructor to print message when object is created.

---
+++ src/OOPs_concept/Demo.java
@@ -0,0 +1,15 @@
+package OOPs_concept;
+
+public class Demo {
+    Demo()                          //Default constructor
+    {
+        System.out.println("Object Created");
+    }
+
+    public static void main(String[] args) {
+
+        Demo d = new Demo();               //d Declares a reference variable & new Demo() Creates object in memory Calls constructor automatically
+    }
+}
+
+
