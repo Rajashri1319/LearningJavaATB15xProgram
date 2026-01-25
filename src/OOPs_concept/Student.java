de1502ba66aba2d7718747a71d4385989578d1dc
Rajashri Tiwari <rajashri.tiwari13@gmail.com>
2026-01-25 20:23:06 +0530

    Create a class with one variable and print it using an object

---
+++ src/OOPs_concept/Student.java
@@ -0,0 +1,13 @@
+package OOPs_concept;
+
+public class Student {                           //Class
+    String name;
+    public static void main(String[] args){
+        Student S = new Student();               // S Declares a reference variable
+             //new Student Creates an object in heap memory & Calls default constructor
+        S.name = "rajashri";
+        System.out.println(S.name);
+
+}
+
+}
